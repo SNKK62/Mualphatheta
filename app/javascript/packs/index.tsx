678d6bc1c4784b9c167b80745a0321b1a6c07c15
SNKK62 <koki.seno62@gmail.com>
2022-01-06 15:14:02 +0900

    1/6

--- app/javascript/packs/index.tsx
+++ app/javascript/packs/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../../assets/stylesheets/index.css';
-import App from '../../assets/javascript/components/App';
+import App from '../components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
